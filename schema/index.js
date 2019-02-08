1f6ffea6a3c96b5ed7f7badee90e6bf896d57236
JIrayut Khantavee <ker.koleng@hotmail.co.th>
2019-02-08 14:25:45 +0700

    Add edit user and delete

--- schema/index.js
+++ schema/index.js
@@ -80,6 +80,29 @@ const Mutation = new GraphQLObjectType({
         return axios.post(`${url}/users`, { ...args }).then(resp => resp.data)
       },
     },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      resolve(source, args) {
+        return axios
+          .patch(`${url}/users/${args.id}`, { ...args })
+          .then(resp => resp.data)
+      },
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(source, { id }) {
+        return axios.delete(`${url}/users/${id}`).then(resp => resp.data)
+      },
+    },
   },
 })
 
